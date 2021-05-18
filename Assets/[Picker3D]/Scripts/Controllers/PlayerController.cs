058fdff2e55d634750269af970fe31f6dd0cc7a3
Arif <arifgider2@gmail.com>
2021-05-18 16:38:50 +0300

    Player Movement Added

---
+++ Assets/[Picker3D]/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerController : Singleton<PlayerController>
+{
+    #region Variables
+
+    [SerializeField] private float _playerSpeed;
+    [SerializeField] private float _forceLimit;
+
+    private Rigidbody _rigidBody;
+    private Rigidbody _Rigidbody { get { return _rigidBody == null ? _rigidBody = GetComponent<Rigidbody>() : _rigidBody; }}
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    private void FixedUpdate()
+    {
+        MoveForward();
+    }
+
+    #endregion
+
+    #region Other Methods
+
+    // Public Methods
+
+    public void Slide(float _value)
+    {
+        _value = Mathf.Clamp(_value, -_forceLimit, _forceLimit);
+        _Rigidbody.AddForce(_value * transform.right);
+    }
+
+    // Private Methods
+
+    private void MoveForward()
+    {
+        _Rigidbody.velocity = transform.forward * _playerSpeed * Time.fixedDeltaTime;
+    }
+
+    #endregion
+}
---
+++ Assets/[Picker3D]/Scripts/Managers/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _smoothness;
+
+    private float _firstValue;
+    private float _lastValue;
+    private float _distance;
+    private float _calculatedValue;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    private void Update()
+    {
+        ControllInput();
+    }
+
+    #endregion
+
+    #region Other Methods
+
+    private void ControllInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+            _firstValue = Input.mousePosition.x;
+        else if (Input.GetMouseButton(0))
+        {
+            _lastValue = Input.mousePosition.x;
+            _distance = _lastValue - _firstValue;
+            _calculatedValue = (_distance / Screen.width) * _smoothness;
+
+            // Movement
+            PlayerController.Instance.Slide(_calculatedValue);
+            
+            _firstValue = _lastValue;
+        }
+    }
+
+    #endregion
+}
